c9523cc5a20036d6cf525790e124228a57a45151
Tommy <tooto1985@gmail.com>
2018-06-26 11:41:19 +0800

    modify

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-var tools = require("./public/tools");
-var express = require("express");
-var path = require("path");
-var app = express();
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/api", function(req, res) {
-	tools.yourfunction();
-    res.send(tools.version);
-});
-app.listen(process.env.PORT || 3000);
+
+var tools = require('./public/tools')
+var express = require('express')
+var bodyParser = require('body-parser')
+var path = require('path')
+var app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({extended: false}))
+app.get('/api', function (req, res) {
+  tools.yourfunction()
+  res.send(tools.version)
+})
+app.get('/send', function (req, res) {
+  if (tools.checkdata(req.query.num)) {
+    res.send('成功')
+  } else {
+    res.send('失敗')
+  }
+})
+app.listen(process.env.PORT || 3000)
--- public/tools.js
+++ public/tools.js
@@ -1,33 +1,21 @@
-(function(tools) {
-	// Node.js
-	if (typeof module === 'object' && module.exports) {
-	    module.exports = tools;
-	}
-	// AMD / RequireJS
-	else if (typeof define === 'function' && define.amd) {
-	    define([], function () {
-	        return tools;
-	    });
-	}
-	// included directly via <script> tag
-	else {
-		var root = typeof self === 'object' && self.self === self && self || typeof global === 'object' && global.global === global && global || this;
-	    root[tools.name] = tools;
-	}	
+(function (tools) {
+  if (typeof module === 'object' && module.exports) { // Node.js
+    module.exports = tools
+  } else if (typeof define === 'function' && define.amd) { // AMD / RequireJS
+    define([], function () {
+      return tools
+    })
+  } else { // included directly via <script> tag
+    var root = typeof self === 'object' && self.self === self && self || typeof global === 'object' && global.global === global && global || this
+    root[tools.name] = tools
+  }
 })({
-	name:"tools", // global name for via <script> tag
-	version: "0.0.2",
-	yourfunction: function() {
-		console.log("hello world");
-	}
-});
-
-
-
-
-
-
-
-
-
-
+  name: 'tools', // global name for via <script> tag
+  version: '0.0.2',
+  yourfunction: function () {
+    console.log('hello world')
+  },
+  checkdata: function (num) {
+    return /^[0-9]*$/.test(num)
+  }
+})
