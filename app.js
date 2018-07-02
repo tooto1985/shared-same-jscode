d6b71b4ccc5d9fd94fbc71c8fb6b665dc8e60b73
Gitea <gitea@fake.local>
2018-07-02 15:18:44 +0800

    modify

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 
+var mongodbUri = "mongodb://127.0.0.1:27017/mydb";
+var collectionName = "users";
+var Db = require("./db");
+var db = new Db(mongodbUri,collectionName);
 var tools = require('./public/tools')
 var express = require('express')
 var bodyParser = require('body-parser')
@@ -11,7 +15,11 @@ app.get('/version', function (req, res) {
 })
 app.post('/send', function (req, res) {
   if (tools.checkCName(req.body.CName) && tools.checkNumber(req.body.Number)) {
-    res.send('成功')
+    db.insert({CName:req.body.CName,Number:req.body.Number},function() {
+      res.send('成功')
+    }, function() {
+      res.send('失敗')
+    })
   } else {
     res.send('輸入內容有誤')
   }
